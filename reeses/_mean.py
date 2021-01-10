e67c08c5058c90ea20997f271017e9da6669d384
Mark Weiss <mark.weiss.mof@gmail.com>
2021-01-09 16:39:24 -0800

    .

--- reeses/_mean.py
+++ reeses/_mean.py
@@ -4,10 +4,9 @@ import numpy as np
 
 class MeanEstimator(BaseEstimator):
     def fit(self, X, y=None, **fit_params):
-        self.mu_ = y.mean(axis=1)
+        self.mu_ = y.mean()
         return self
 
     def predict(self, X):
-        if len(shape) > 1:
-            _shape = (len(X), *shape[1:])
-        return np.ones((len(X), _shape)) * self.mu
+        output = np.ones((len(X),)) * self.mu_
+        return output
