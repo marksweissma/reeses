50fe4948ea7a67a7128b0ed10cca474ce63fb738
Mark Weiss <mark.weiss.mof@gmail.com>
2021-01-20 22:37:35 -0800

    add attrs dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(name='reeses',
-      version='0.0.2',
+      version='0.0.3',
       author='Mark Weiss',
       author_email='mark.s.weiss.ma@gmail.com',
       description=open('README.md'),
       url='http://github.com/marksweissma/reeses',
       packages=find_packages(),
-      install_requires=['sklearn', 'numpy'],
+      install_requires=['sklearn', 'numpy', 'attrs'],
       summary='sklearn piecewise modeling plugin',
       license='MIT',
       zip_safe=False
